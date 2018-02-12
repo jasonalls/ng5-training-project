f6e8c47c8429eaadc3f253b14c314ef94c6f19e5
Jason William Alls <jasonalls@hotmail.com>
2018-02-12 20:46:19 +0000

    First commit.

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number;
+  // tslint:disable-next-line:no-inferrable-types
+  btnText: string = 'Add an item';
+  // tslint:disable-next-line:no-inferrable-types
+  goalText: string = 'My first life goal';
+  goals = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount = this.goals.length;
+  }
+
+}
